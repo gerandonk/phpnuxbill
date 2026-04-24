63dd81f8bd5f0a8a48e5f0bbdcbe9c7ad6a7bdf3
Md. Atik <atik49720@gmail.com>
2026-04-24 09:46:59 +0600

    Fixed admin logout redirection
    When admin logged out, it redirecting to customer login page which is quite disturbing to an admin. So that i took a isAdmin flag, if the logged in user is a admin user then redirecting to the admin login.

--- system/controllers/logout.php
+++ system/controllers/logout.php
@@ -9,9 +9,16 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
 header("Pragma: no-cache");
 
-run_hook('customer_logout'); #HOOK
+$isAdmin = false;
 if (session_status() == PHP_SESSION_NONE) session_start();
+if($_SESSION['aid'] != NULL){
+    $isAdmin = true;
+}
+run_hook('customer_logout'); #HOOK
 Admin::removeCookie();
 User::removeCookie();
 session_destroy();
+if($isAdmin){
+    _alert(Lang::T('Logout Successful'), 'warning', "admin");
+}
 _alert(Lang::T('Logout Successful'), 'warning', "login");
